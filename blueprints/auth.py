5ffc32abf53672d77c1f8bc5ac701a3d510e36c4
JV-X <xjv1195275315@gmail.com>
2023-05-27 21:32:41 +0800

    登录功能后端实现

--- blueprints/auth.py
+++ blueprints/auth.py
@@ -1,19 +1,35 @@
 import random
 import string
 
-from flask import Blueprint, render_template, request, jsonify,redirect,url_for
+from flask import Blueprint, render_template, request, jsonify, redirect, url_for, session
 from exts import mail, db
 from flask_mail import Message
 from models import EmailCaptchaModel, UserModel
-from .form import RegisterForm
-from werkzeug.security import generate_password_hash
+from .form import RegisterForm, LoginForm
+from werkzeug.security import generate_password_hash, check_password_hash
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 
-@bp.route('/login')
+@bp.route('/login', methods=['GET', 'POST'])
 def login():
-    return 'login'
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        form = LoginForm(request.form)
+        if form.validate():
+            email = form.email.data
+            password = form.password.data
+            user = UserModel.query.filter_by(email=email).first()
+            if not user:
+                print('邮箱不存在！')
+                return redirect(url_for('auth.login'))
+            if check_password_hash(user.password, password):
+                session['user_id'] = user.id
+                return redirect('/')
+            else:
+                print('密码错误')
+                return redirect(url_for('auth.login'))
 
 
 @bp.route('/register', methods=['GET', 'POST'])
@@ -26,7 +42,7 @@ def register():
             email = form.email.data
             username = form.username.data
             password = form.password.data
-            user = UserModel(email=email,username=username,password=generate_password_hash(password))
+            user = UserModel(email=email, username=username, password=generate_password_hash(password))
             db.session.add(user)
             db.session.commit()
             return redirect(url_for('auth.login'))
--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+SECRET_KEY = 'aasdasddfgwghwrrh'
+
 HOSTNAME = '127.0.0.1'
 PORT = '3306'
 DATABASE = 'zhiliaooa'
