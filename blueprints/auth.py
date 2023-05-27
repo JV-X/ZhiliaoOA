7d06cd50801e2af1ffc02548ab03f6acf74df961
JV-X <xjv1195275315@gmail.com>
2023-05-27 15:10:56 +0800

    注册页面模板渲染

--- blueprints/auth.py
+++ blueprints/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint,render_template
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -6,3 +6,7 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 @bp.route('login')
 def login():
     pass
+
+@bp.route('register')
+def register():
+    return render_template('register.html')
