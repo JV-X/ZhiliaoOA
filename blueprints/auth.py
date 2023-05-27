749736de8f85c19b4f1582821d4b7e3969803d76
JV-X <xjv1195275315@gmail.com>
2023-05-27 16:08:16 +0800

    邮件发送功能实现（后端部分）

--- blueprints/auth.py
+++ blueprints/auth.py
@@ -1,6 +1,10 @@
-from flask import Blueprint,render_template
-from exts import mail
+import random
+import string
+
+from flask import Blueprint, render_template, request, jsonify
+from exts import mail, db
 from flask_mail import Message
+from models import EmailCaptchaModel
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -9,12 +13,22 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 def login():
     pass
 
+
 @bp.route('/register')
 def register():
     return render_template('register.html')
 
-@bp.route('/mail/test')
-def mail_test():
-    message = Message(subject='测试',recipients=['xjv1195275315@qq.com'],body='测试邮件')
+
+@bp.route('/captcha/email')
+def get_email_captcha():
+    email = request.args.get('email')
+    source = string.digits * 4
+    captcha = random.sample(source, 4)
+    captcha = ''.join(captcha)
+    message = Message(subject='ZhiliaoOA验证码', recipients=[email], body=f'您的验证码是{captcha}')
     mail.send(message)
-    return 'success'
+
+    email_captcha = EmailCaptchaModel(email=email, captcha=captcha)
+    db.session.add(email_captcha)
+    db.session.commit()
+    return jsonify({'code': 200, 'message': '', 'data': None})
