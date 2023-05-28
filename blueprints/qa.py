74cd40503dd9ad233cbb5e6a7169b75ef217a216
JV-X <xjv1195275315@gmail.com>
2023-05-28 11:12:20 +0800

    发布问答页面渲染

--- blueprints/qa.py
+++ blueprints/qa.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint,request,render_template
 
 bp = Blueprint('qa', __name__, url_prefix='/')
 
@@ -6,3 +6,9 @@ bp = Blueprint('qa', __name__, url_prefix='/')
 @bp.route('/')
 def index():
     return 'Hello World!'
+
+
+@bp.route('qa/public', methods=['GET', "POST"])
+def public_qa():
+    if request.method == 'GET':
+        return render_template('public_question.html')
