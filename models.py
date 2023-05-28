b92ffce3af13fabe98d71f7894aa2c788f4d0eee
JV-X <xjv1195275315@gmail.com>
2023-05-28 20:39:21 +0800

    答案模型创建

--- models.py
+++ models.py
@@ -28,3 +28,16 @@ class QuestionModel(db.Model):
 
     author_id = db.Column(db.Integer, db.ForeignKey("user.id"))
     author = db.relationship(UserModel, backref="questions")
+
+
+class AnswerModel(db.Model):
+    __tablename__ = 'answer'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    content = db.Column(db.Text, nullable=False)
+    create_time = db.Column(db.DateTime, default=datetime.now)
+
+    question_id = db.Column(db.Integer, db.ForeignKey('question.id'))
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    question = db.relationship(QuestionModel, backref=db.backref('answers', order_by=create_time.desc()))
+    author = db.relationship(QuestionModel, backref="answers")
